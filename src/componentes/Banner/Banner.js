c4fa4e9bd4941a42e70337cfba9cdc9af73da941
Heitor <heitor.moreira.neto@outlook.com>
2024-10-08 08:51:38 -0300

    desenvolvimento do projeto

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+function Banner() {
+    return(
+        <header className='banner'>
+            <img src="/imagens/banner.png" alt="O banner principal da pagina organo"/>
+        </header>
+    )
+}
+
+export default Banner
---
+++ src/componentes/Formulario/index.js
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+import Botao from '../Botao'
+import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
+import './Formulario.css'
+
+const Formulario = () => {
+    const times = [
+        'point guard (PG)',
+        'shooting guard (SG)',
+        'small forward (SF)',
+        'power forward (PF)',
+        'center (C)'
+    ]
+
+    const [nome, setNome] = useState('')
+    const [cargo, setCargo] = useState('')
+    const [imagem, setImagem] = useState('')
+    const [posicao, setPosicao] = useState('')
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        console.log('Form foi submetido => ', nome, cargo, imagem)
+    }
+
+    return (
+        <section className='Formulario'>
+            <form onSubmit={aoSalvar}>
+                <h2>Preencha os dados para criar o card do colaborador</h2>
+                <CampoTexto 
+                    obrigatorio={true} 
+                    label="Nome" 
+                    placeholder="Digite seu nome"
+                    valor={nome}
+                    aoAlterado={valor => setNome(valor)}
+                />
+                <CampoTexto 
+                    obrigatorio={true} 
+                    label="Cargo" 
+                    placeholder="Digite seu cargo"
+                    valor={cargo}
+                    aoAlterado={valor => setCargo(valor)}
+                />
+                <CampoTexto 
+                    label="Imagem" 
+                    placeholder="Digite o endereço da imagem"
+                    valor={imagem}
+                    aoAlterado={valor => setImagem(valor)}
+                />
+                <ListaSuspensa 
+                    obrigatorio={true} 
+                    label="Posição" 
+                    itens={times}
+                    valor={posicao}
+                    aoAlterado={valor => setPosicao(valor)}
+                />
+                <Botao>
+                    Criar card
+                </Botao>
+            </form>
+        </section>
+    )
+}
+
+export default Formulario
